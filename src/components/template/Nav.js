4a1700db597f5950667fd20efb0edd83073d9539
Dennis-Lord <dennislord2001@gmail.com>
2021-10-29 14:14:11 +0000

    Updates to Nav and mid to footer homepage

--- src/components/template/Nav.js
+++ src/components/template/Nav.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import "../../styles/nav.css"
+
+// nav and nav secion of home or main page
 export default function Nav() {
-  return (
+  return (<>
     <div className="nav-wrapper">
       <p className="nav-title">JOB COACH</p>
+      <div className="settings-icon">Settings</div>
     </div>
+    <section className="nav-section">
+      <p className="greeting-message">Hello</p>
+      <p className="username">Username</p>
+    </section>
+    </>
   )
 }
---
+++ src/components/template/midToFooter.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "../../styles/midToFooter.css";
+
+// half of screen to footer section or midToFooter section of main page
+const midToFooter = () => {
+    return (<>
+    <div className="midToFooter">
+
+    </div>
+    <div clasName="ai-chatbot-button">
+        chat with AI
+    </div>
+        </>
+     );
+}
+ 
+export default midToFooter;
