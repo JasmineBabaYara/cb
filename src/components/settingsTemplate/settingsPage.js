15065a2af8054682c6d0ffc74104ca39e8d09ae8
JasmineBabaYara <jassyprincess1@gmail.com>
2021-10-30 17:16:21 -0700

    updated settingspage, nav, midtofooter

--- src/components/settingsTemplate/settingsPage.js
+++ src/components/settingsTemplate/settingsPage.js
@@ -1,2 +1,40 @@
-import React from 'react';
+import React, { Component } from "react";
+import "../../styles/settingspage.css";
+
+
+// settings page
+export default class SettingsPage extends Component {
+  render(){
+  return (
+    <div className="settings">
+      <div className="setting-img"></div>
+      <p className="account-title">ACCOUNT SETTING</p>
+      <div className="account-info">
+        <div className="name-div">
+          <p className="name">Set Full Name</p>
+          <button className="name-btn">Save</button>
+        </div>
+        <div className="contact-div">
+          <p className="contact">Set Contact</p>
+          <button className="contact-btn">Save</button>
+        </div>
+        <div className="category-div">
+          <p className="category">Select Category</p>
+          <button className="category-btn">Save</button>
+        </div>
+        <div className="notifications-div">
+          <p className="notifications">Allow Notifications</p>
+          <button className="notifications-btn">On/Off</button>
+        </div>
+        <p className="faqs">FAQS</p>
+        <p className="feedback">SEND FEEDBACK</p>
+        <p className="about">ABOUT</p>
+        <p className="reset">RESET SETTINGS</p>
+      </div>
+        <button className="logout-btn">LOGOUT</button>
+    </div>
+  )
+  }
+}
+
 
--- src/components/template/midToFooter.js
+++ src/components/template/midToFooter.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React from "react";
 import "../../styles/midToFooter.css";
 
 // half of screen to footer section or midToFooter section of main page
 const midToFooter = () => {
-    return (<>
-    <div className="midToFooter">
-
-    </div>
+  return (
+    <>
+      <div className="midToFooter">
+        <p className="midToFooter-title">available jobs </p>
+        <div className="img-div">AI IMG</div>
+      </div>
+      {/*
     <div clasName="ai-chatbot-button">
         chat with AI
-    </div>
-        </>
-     );
-}
- 
+    </div>*/}
+    </>
+  );
+};
+
 export default midToFooter;
