027a0f28b778c0f96e1bb0182a891c90ffc911c0
Dennis-Lord <dennislord2001@gmail.com>
2021-11-01 20:42:05 +0000

    Settings page completed with terrible colors

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
 import "./App.css";
+import SettingsPage from "./components/settingsTemplate/settingsPage";
 
 
 //Import Components
-import Courses from "./components/template/Courses";
-import MidToFooter from "./components/template/midToFooter";
-import Nav from "./components/template/Nav";
+// import Courses from "./components/template/Courses";
+// import MidToFooter from "./components/template/midToFooter";
+// import Nav from "./components/template/Nav";
 
 
 function App() {
   return (<>
-      <Nav />
+      {/* <Nav />
       <Courses />
-      <MidToFooter />
+      <MidToFooter /> */}
+      <SettingsPage />
       </>
   );
 }
--- src/components/settingsTemplate/settingsPage.js
+++ src/components/settingsTemplate/settingsPage.js
@@ -6,32 +6,41 @@ import "../../styles/settings/settingspage.css";
 export default class SettingsPage extends Component {
   render(){
   return (
-    <div className="settings">
+    <div className="settings-container">
+      <div className="return-btn">
+      <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
+</svg>       
+      </div>
+      <div className="s-img-div">
       <div className="setting-img"></div>
+      </div>
       <p className="account-title">ACCOUNT SETTING</p>
       <div className="account-info">
-        <div className="name-div">
-          <p className="name">Set Full Name</p>
-          <button className="name-btn">Save</button>
+        <div className="div">
+          <input placeholder="Set Full Name"/>
+          <button className="btn">Save</button>
         </div>
-        <div className="contact-div">
-          <p className="contact">Set Contact</p>
-          <button className="contact-btn">Save</button>
+        <div className="contact-div div">
+          <input placeholder="Set contact" />
+          <button className="btn">Save</button>
         </div>
-        <div className="category-div">
-          <p className="category">Select Category</p>
-          <button className="category-btn">Save</button>
+        <div className="div">
+          <input  placeholder="Choose category" />
+          <button className="btn">Save</button>
         </div>
+        </div>
+        <div className="optional-settings">
         <div className="notifications-div">
           <p className="notifications">Allow Notifications</p>
-          <button className="notifications-btn">On/Off</button>
+          <button className="notifications-btn">On</button>
         </div>
         <p className="faqs">FAQS</p>
         <p className="feedback">SEND FEEDBACK</p>
         <p className="about">ABOUT</p>
-        <p className="reset">RESET SETTINGS</p>
       </div>
-        <button className="logout-btn">LOGOUT</button>
+      <div className="footer-div"><button className="reset">RESET</button>
+        <button className="logout-btn">LOGOUT</button></div>
     </div>
   )
   }
